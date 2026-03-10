1a720d5344f74ba234b2b3392ef05c98be6d32de
20230237-hash <20230237@eaut.edu.vn>
2026-03-10 10:38:29 +0700

    Add delete functionality for brands

---
+++ admin/brands/delete.php
@@ -0,0 +1,14 @@
+<?php
+include("../../config/database.php");
+include("../check_admin.php");
+
+$id = intval($_GET['id']);
+
+$conn->query("
+DELETE FROM brands
+WHERE id=$id
+");
+
+header("Location:list.php");
+exit();
+?>
